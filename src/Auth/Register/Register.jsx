c01aa9a1bd1c4487807ab52568b8b624f4703b90
Sivananthan Subbramaniam <126233811+Siva-1998-code@users.noreply.github.com>
2024-02-16 15:36:51 +0530

    Register.jsx

--- src/Auth/Register/Register.jsx
+++ src/Auth/Register/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
   }, [])
   const handleSubmit = async (e) => {
     e.preventDefault()
-    let phoneRegex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/gm;
+    let phoneRegex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/gm;
     let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     try {
       if (!credentials.email && !credentials.firstName && !credentials.password && !credentials.phoneNumber && !credentials.lastName) {
